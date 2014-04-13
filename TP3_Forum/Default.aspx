7b19db38f0e29f2b144f1ddad913689e8a89ec1b
Jean-David <jeandavidmoisan@gmail.com>
2014-04-13 16:28:50 -0400

    Tried to get the asp part started.

---
+++ TP3_Forum/Default.aspx
@@ -0,0 +1,6 @@
+﻿<%@ Page Title="First page" Language="C#" MasterPageFile="~/TP3.Master" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="TP3_Forum.Default" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="PageContent" runat="server">
+
+</asp:Content>
---
+++ TP3_Forum/Default.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TP3_Forum
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        private OleDbConnection getDatabaseConnection()
+        {
+            OleDbConnection oleDbConnection = new OleDbConnection();
+            oleDbConnection.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + Server.MapPath(@"assets/database/Examen2.accdb");
+            oleDbConnection.Open();
+            return oleDbConnection;
+        }
+
+        private void executeNonQuery(string query, Label lblResult)
+        {
+            OleDbConnection oleDbConnection = null;
+            try
+            {
+                oleDbConnection = getDatabaseConnection();
+                OleDbCommand oleDbCommand = new OleDbCommand(query, oleDbConnection);
+                lblResult.Text = "Commande exécutée: " + oleDbCommand.ExecuteNonQuery() + " lignes affectées";
+            }
+            catch (Exception ex)
+            {
+                lblResult.Text = "Commande ratée: " + ex.Message;
+            }
+            finally
+            {
+                if (oleDbConnection != null)
+                {
+                    oleDbConnection.Close();
+                }
+            }
+        }
+    }
+}
